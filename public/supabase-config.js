734440a70a2126722f04c52a8aee965116d972db
ivnsto <ivan.soteros@imss.gob.mx>
2026-08-22 13:46:58 -0600

    Configurar conexión con Supabase

--- public/supabase-config.js
+++ public/supabase-config.js
@@ -1,2 +1,2 @@
-window.SUPABASE_URL = "";
-window.SUPABASE_ANON_KEY = "";
+window.SUPABASE_URL = "https://dpecpbhtqdrcldkprpio.supabase.co";
+window.SUPABASE_ANON_KEY = "sb_publishable_4KycwJLUrE_Noa2xKin1Xw_0Z8lV5jo";
